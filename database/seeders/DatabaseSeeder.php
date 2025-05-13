84cad84a9ced43d9df608dec0c6b0390e5e8f02a
Ren <sergeysolovyov2016@gmail.com>
2025-05-13 12:14:21 +0000

    - update user factory to include surname, phone, and address fields - modify category seeder to use correct factory method - add user seeder for creating user records - update database seeder to call user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([CategorySeeder::class]);
+        $this->call([
+            CategorySeeder::class,
+            UserSeeder::class
+        ]);
     }
 }
