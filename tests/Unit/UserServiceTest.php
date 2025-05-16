8a4b5c1156aa2bb73c4a8caee778b6558e0bc1dc
Ren <sergeysolovyov2016@gmail.com>
2025-05-16 19:39:10 +0000

    - refactors user service tests for better structure and clarity - adds tests for updating and deleting users - improves mocking of requests and user resources

--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -1,12 +1,19 @@
 <?php
 
+namespace Tests\Unit;
+
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Http\Services\UserService;
+use App\Models\User;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\Auth;
+use Mockery;
+use Mockery\MockInterface;
 use Ren\Http\Services\Utils\AuthUtil;
 
-test('create user and returns resource(mocking)', function () {
-    // Arrange
+test("create user and returns resource(mock)", function () {
     $userData = [
         "name" => "Billy",
         "surname" => "Herrington",
@@ -14,9 +21,8 @@ test('create user and returns resource(mocking)', function () {
         "phone" => "+380123456789",
         "email" => "sergeysolovyov2016@gmail.com",
     ];
-
     $request = $this->mock(StoreUserRequest::class);
-    $request->shouldReceive('input')->with(Mockery::anyOf(
+    $request->shouldReceive("input")->with(Mockery::anyOf(
         "name",
         "surname",
         "email",
@@ -26,17 +32,12 @@ test('create user and returns resource(mocking)', function () {
         "contacts",
         "addresses",
     ))->andReturnUsing(fn($key) => array_key_exists($key, $userData) ? $userData[$key] : null);
-
     $userService = new UserService($this->mock(AuthUtil::class));
-
-    // Act
     $result = $userService->createUser($request);
-
-    // Assert
     expect($result)->toBeInstanceOf(UserResource::class);
 });
 
-test('create user and returns resource(object)', function () {
+test("create user and returns resource(object)", function () {
     // Arrange
     $userData = [
         "name" => "Billy",
@@ -45,22 +46,23 @@ test('create user and returns resource(object)', function () {
         "phone" => "+380123456789",
         "email" => "sergeysolovyov2016@gmail.com",
     ];
-
     $request = new StoreUserRequest(request: $userData, server: ["REQUEST_METHOD" => "POST"]);
-
     $userService = new UserService(new AuthUtil());
-
-    // Act
     $result = $userService->createUser($request);
-
-    // Assert
     expect($result)->toBeInstanceOf(UserResource::class);
 });
 
-function mockStoreUserRequest(&$context, $userData)
-{
-    $request = $context->mock(StoreUserRequest::class);
-    $request->shouldReceive('input')->with(Mockery::anyOf(
+test("update user and returns resource(mock)", function () {
+    $user = User::factory()->create();
+    $userData = [
+        "name" => "Billy",
+        "surname" => "Herrington",
+        "password" => "qwertyuiop12345$",
+        "phone" => "+380123456789",
+        "email" => "sergeysolovyov2016@gmail.com",
+    ];
+    $request = $this->mock(UpdateUserRequest::class);
+    $request->shouldReceive("input")->with(Mockery::anyOf(
         "name",
         "surname",
         "email",
@@ -70,5 +72,103 @@ function mockStoreUserRequest(&$context, $userData)
         "contacts",
         "addresses",
     ))->andReturnUsing(fn($key) => array_key_exists($key, $userData) ? $userData[$key] : null);
-    return $request;
-}
+    $request->shouldReceive("whenHas")->with(Mockery::anyOf(
+        "name",
+        "surname",
+        "email",
+        "password",
+        "phone",
+        "image_url",
+        "contacts",
+        "addresses",
+    ), Mockery::type("Closure"))->andReturnUsing(fn($key, $callable) => array_key_exists($key, $userData) ? $userData[$key] : $callable());
+    $auth = $this->mock(AuthUtil::class);
+    $auth->shouldReceive("checkUserAffiliation");
+    $userService = new UserService($auth);
+    $result = $userService->updateUser($request, $user);
+    expect($result)->toBeInstanceOf(UserResource::class);
+});
+
+test("update user and returns resource(object)", function () {
+    $user = User::factory()->create();
+    $userData = [
+        "name" => "Billy",
+        "surname" => "Herrington",
+        "password" => "qwertyuiop12345$",
+        "phone" => "+380123456789",
+        "email" => "sergeysolovyov2016@gmail.com",
+    ];
+    $request = new UpdateUserRequest(request: $userData, server: ["REQUEST_METHOD" => "POST"]);
+    Auth::shouldReceive("user")->andReturn($user);
+    $userService = new UserService(new AuthUtil());
+    $result = $userService->updateUser($request, $user);
+    expect($result)->toBeInstanceOf(UserResource::class);
+});
+
+// test("get list user as resource collection(mock)", #[RunInSeparateProcess, PreserveGlobalState(false)] function () {
+//     $paginator = $this->mock(LengthAwarePaginator::class, function (MockInterface $mock) {
+//         $mock->shouldReceive("toResourceCollection")->andReturn(ResourceCollection::class);
+//     });
+//     $user = $this->mock("alias:" . User::class);
+//     $user->shouldReceive("paginate")->andReturn($paginator);
+//     $userService = new UserService($this->mock(AuthUtil::class));
+//     $result = $userService->listUserResources();
+//     expect($result)->toBeInstanceOf(ResourceCollection::class);
+// });
+
+test("get list user as resource collection(object)", function () {
+    User::factory()->count(5)->create();
+    $userService = new UserService(new AuthUtil());
+    $result = $userService->listUserResources();
+    expect($result)->toBeInstanceOf(ResourceCollection::class);
+});
+
+test("get user as resource(mock)", function () {
+    $user = $this->mock(User::class);
+    $user->shouldReceive("toResource")->andReturn($this->mock(UserResource::class));
+    $userService = new UserService($this->mock(AuthUtil::class));
+    $result = $userService->getUserResource($user);
+    expect($result)->toBeInstanceOf(UserResource::class);
+});
+
+test("get user as resource(object)", function () {
+    $user = User::factory()->create();
+    $userService = new UserService(new AuthUtil());
+    $result = $userService->getUserResource($user);
+    expect($result)->toBeInstanceOf(UserResource::class);
+});
+
+test("delete user and return resource(mock)", function () {
+    $user = $this->mock(User::class);
+    $user->shouldReceive("delete");
+    $user->shouldReceive("toResource")->andReturn($this->mock(UserResource::class));
+    $auth = $this->mock(AuthUtil::class);
+    $auth->shouldReceive("checkUserAffiliation");
+    $userService = new UserService($auth);
+    $result = $userService->deleteUser($user);
+    expect($result)->toBeInstanceOf(UserResource::class);
+});
+
+test("delete user and return resource(object)", function () {
+    $user = User::factory()->create();
+    Auth::shouldReceive("user")->andReturn($user);
+    $userService = new UserService(new AuthUtil());
+    $result = $userService->deleteUser($user);
+    expect($result)->toBeInstanceOf(UserResource::class);
+});
+
+// class UserServiceTest extends TestCase
+// {
+//     #[RunInSeparateProcess]
+//     public function get_list_user_as_resource_collection_mock()
+//     {
+//         $paginator = $this->mock(LengthAwarePaginator::class, function (MockInterface $mock) {
+//             $mock->shouldReceive("toResourceCollection")->andReturn(ResourceCollection::class);
+//         });
+//         $user = $this->mock("alias:" . User::class);
+//         $user->shouldReceive("paginate")->andReturn($paginator);
+//         $userService = new UserService($this->mock(AuthUtil::class));
+//         $result = $userService->listUserResources();
+//         expect($result)->toBeInstanceOf(ResourceCollection::class);
+//     }
+// }
